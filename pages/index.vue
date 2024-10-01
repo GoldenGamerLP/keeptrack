1df2bdb8a30556fb45808edc8ae4cbd8988770c6
GoldenGamer_LP <alexanderweishaupt970@gmail.com>
2024-10-01 20:38:54 +0200

    bug fix test

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="h-screen flex flex-col justify-between items-center">
         <header class="max-w-sm mt-32">
             <Icon name="lucide:coins" class="animate-bounce" size="50" />
-            <h1 class="text-4xl font-semibold text-primary my-2">KeepTrack</h1>
+            <h1 class="text-2xl font-bold text-primary">KeepTrack</h1>
             <p class="text-lg max-w-sm">Halte deine Zeiten, Gehalt, Minojob im blick!</p>
             <p class="text-sm text-muted-foreground" v-if="!isMobile">Diese App funkioniert nur auf dem Handy.</p>
             <p v-if="isMobile && (pwaInstallSupported && !hasPrompt)" class="text-sm">
@@ -11,23 +11,26 @@
                     Um die App zu installieren, klicke auf das Teilen-Symbol und wähle "Zum Home-Bildschirm" aus.
                 </template>
                 <template v-else>
-                    Um die App zu installieren, klicke auf Mehr oder das Teilen-Symbol und wähle "Zum Startbildschirm
-                    hinzufügen" aus.
+                    Um die App zu installieren, klicke auf Mehr oder das Teilen-Symbol und wähle "Zum Startbildschirm hinzufügen" aus.
                 </template>
             </p>
 
         </header>
-        <footer class="mx-2 w-full mb-2 space-y-2 max-w-sm sm:max-w-md">
-            <Button class="w-full" @click="requestInstall" v-if="pwaInstallSupported"
-                :variant="(hasPrompt && isMobile) ? 'default' : 'destructive'" :disabled="!isMobile">
+        <footer class="mx-2 w-full mb-2 space-y-2 max-w-md sm:max-w-sm">
+            <Button class="w-full" @click="requestInstall" v-if="pwaInstallSupported" :variant="(hasPrompt && isMobile) ? 'default' : 'destructive'" :disabled="!isMobile">
                 <Icon :name="(hasPrompt && isMobile) ? 'mdi:download' : 'mdi:close'" class="mr-2" />
                 {{ (isMobile && pwaInstallSupported && hasPrompt) ? 'Installieren' : 'Lese dir die Anleitung durch' }}
             </Button>
-            <Button variant="outline" class="w-full" :disabled="!isMobile" v-if="!pwaInstallSupported && !hasPrompt">
+            <Button variant="outline" class="w-full" :disabled="!isMobile" v-if="!(pwaInstallSupported && hasPrompt)">
                 <NuxtLink to="/authentication">
                     Ohne Installation fortfahren (nicht empfohlen)
                 </NuxtLink>
             </Button>
+            <p>debug</p>
+            <p>useragent: {{ userAgent }}</p>
+            <p>isMobile: {{ isMobile }}</p>
+            <p>pwaInstallSupported: {{ pwaInstallSupported }}</p>
+            <p>hasPrompt: {{ hasPrompt }}</p>
         </footer>
     </div>
 </template>
@@ -37,7 +40,7 @@ import type { BeforeInstallPromptEvent } from '@vite-pwa/nuxt/dist/runtime/plugi
 import { useMediaQuery } from '@vueuse/core';
 
 const deferredPrompt = ref<BeforeInstallPromptEvent | null>(null);
-const pwaInstallSupported = ref<boolean>(false)
+const pwaInstallSupported = ref<boolean>(false);
 const hasPrompt = computed(() => !!deferredPrompt);
 const isMobile = useMediaQuery('(any-pointer:coarse) and (orientation:portrait)');
 const userAgent = ref<string>();
@@ -56,6 +59,7 @@ onBeforeRouteUpdate(() => {
     if (window.matchMedia('(display-mode: standalone)').matches) {
         useRouter().push('/authentication');
     }
+
     window.addEventListener('beforeinstallprompt', (e) => {
         e.preventDefault();
         deferredPrompt.value = e as BeforeInstallPromptEvent;
