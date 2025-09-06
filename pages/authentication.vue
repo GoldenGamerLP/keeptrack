713f5f36e1ca56e20153a404ccf41e9515d39fdc
GoldenGamerLP <65727384+GoldenGamerLP@users.noreply.github.com>
2025-09-06 08:44:04 +0200

    refactor: update dependencies and improve authentication page layout
    - Updated package dependencies to latest versions, including Nuxt and Pinia.
    - Refactored authentication.vue for improved readability and consistency.
    - Enhanced error handling with toast notifications for registration and login failures.
    - Fixed minor typos and improved text formatting in the authentication UI.

--- pages/authentication.vue
+++ pages/authentication.vue
@@ -5,26 +5,27 @@
       <h1 class="text-4xl font-semibold text-primary my-2">KeepTrack</h1>
       <Tabs default-value="register" class="max-w-sm">
         <TabsList class="grid w-full grid-cols-2">
-          <TabsTrigger value="register">
-            Registesrieren
-          </TabsTrigger>
-          <TabsTrigger value="login">
-            Einloggen
-          </TabsTrigger>
+          <TabsTrigger value="register"> Registesrieren </TabsTrigger>
+          <TabsTrigger value="login"> Einloggen </TabsTrigger>
         </TabsList>
         <TabsContent value="register">
           <Card>
             <CardHeader>
-              <CardTitle>Registesrieren
-              </CardTitle>
+              <CardTitle>Registesrieren </CardTitle>
               <CardDescription>
-                Erstelle einen Account um deine Zeiten, Gehalt, Minojob im blick zu behalten.
+                Erstelle einen Account um deine Zeiten, Gehalt, Minojob im blick
+                zu behalten.
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <AutoForm @submit="registerSubmit"
-                :field-config="{ password: { inputProps: { type: 'password' } }, confirmPassword: { inputProps: { type: 'password' } } }"
-                :schema="registerSchema">
+              <AutoForm
+                @submit="registerSubmit"
+                :field-config="{
+                  password: { inputProps: { type: 'password' } },
+                  confirmPassword: { inputProps: { type: 'password' } },
+                }"
+                :schema="registerSchema"
+              >
                 <p class="text-red-500">{{ fetchError }}</p>
                 <Button type="submit" class="mt-2" :loading="isLoading">
                   <Icon name="mdi:account-plus" class="mr-2 size-5" />
@@ -39,12 +40,18 @@
             <CardHeader>
               <CardTitle>Einloggen</CardTitle>
               <CardDescription>
-                Logge dich ein um deine Zeiten, Gehalt, Minojob im blick zu behalten.
+                Logge dich ein um deine Zeiten, Gehalt, Minojob im blick zu
+                behalten.
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <AutoForm :field-config="{ password: { inputProps: { type: 'password' } } }" :schema="loginSchema"
-                @submit="loginSubmit">
+              <AutoForm
+                :field-config="{
+                  password: { inputProps: { type: 'password' } },
+                }"
+                :schema="loginSchema"
+                @submit="loginSubmit"
+              >
                 <p class="text-red-500">{{ fetchError }}</p>
                 <Button type="submit" class="mt-2" :loading="isLoading">
                   <Icon name="mdi:login" class="size-5 mr-2" />
@@ -60,61 +67,101 @@
 </template>
 
 <script lang="ts" setup>
-import * as z from 'zod'
+import * as z from "zod";
+import { toast } from "~/components/ui/toast";
 
 definePageMeta({
-  middleware: 'only-guests',
+  middleware: "only-guests",
 });
 
 useHead({
-  title: 'KeepTrack - Deine Zeiten, Gehalt, Minijob im Blick',
+  title: "KeepTrack - Deine Zeiten, Gehalt, Minijob im Blick",
   meta: [
-    { name: 'description', content: 'Halte deine Zeiten, Gehalt, Minojob im blick!' },
-    { name: 'keywords', content: 'Zeiten, Gehalt, Minijob, Stunden, Lohn' },
+    {
+      name: "description",
+      content: "Halte deine Zeiten, Gehalt, Minojob im blick!",
+    },
+    { name: "keywords", content: "Zeiten, Gehalt, Minijob, Stunden, Lohn" },
   ],
   noscript: [
-    { innerHTML: 'Diese App benötigt JavaScript um zu funktionieren' }
-  ]
+    { innerHTML: "Diese App benötigt JavaScript um zu funktionieren" },
+  ],
 });
 
 const isLoading = ref(false);
-const fetchError = ref('');
+const fetchError = ref("");
 
-watch(() => fetchError.value, (error) => {
-  if (error) {
-    setTimeout(() => {
-      fetchError.value = '';
-    }, 5000);
+watch(
+  () => fetchError.value,
+  (error) => {
+    if (error) {
+      setTimeout(() => {
+        fetchError.value = "";
+      }, 5000);
+    }
   }
-});
+);
 
 const loginSchema = z.object({
-  email: z.string().email('Ungültige E-Mail Adresse').describe("Deine E-Mail Adresse"),
-  password: z.string().min(6, 'Passwort muss mindestens 6 Zeichen lang sein').describe("Dein Passwort"),
+  email: z
+    .string()
+    .email("Ungültige E-Mail Adresse")
+    .describe("Deine E-Mail Adresse"),
+  password: z
+    .string()
+    .min(6, "Passwort muss mindestens 6 Zeichen lang sein")
+    .describe("Dein Passwort"),
 });
 
-const registerSchema = z.object({
-  displayName: z.string().min(3, 'Name muss mindestens 3 Zeichen lang sein').describe("Dein Name"),
-  email: z.string().email('Ungültige E-Mail Adresse').describe("Deine E-Mail Adresse"),
-  password: z.string().min(6, 'Passwort muss mindestens 6 Zeichen lang sein').describe("Dein Passwort"),
-  confirmPassword: z.string().min(6, 'Passwort muss mindestens 6 Zeichen lang sein').describe("Passwort bestätigen"),
-});
+const registerSchema = z
+  .object({
+    displayName: z
+      .string()
+      .min(3, "Name muss mindestens 3 Zeichen lang sein")
+      .describe("Dein Name"),
+    email: z
+      .string()
+      .email("Ungültige E-Mail Adresse")
+      .describe("Deine E-Mail Adresse"),
+    password: z
+      .string()
+      .min(6, "Passwort muss mindestens 6 Zeichen lang sein")
+      .describe("Dein Passwort"),
+    confirmPassword: z
+      .string()
+      .min(6, "Passwort muss mindestens 6 Zeichen lang sein")
+      .describe("Passwort bestätigen"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwörter stimmen nicht überein",
+    path: ["confirmPassword"],
+  });
 
 const registerSubmit = async (values: Record<string, any>) => {
   isLoading.value = true;
   try {
-    const res = await $fetch('/api/v1/auth/actions/signup', {
-      method: 'POST',
-      body: JSON.stringify(values)
+    const res = await $fetch("/api/v1/auth/actions/signup", {
+      method: "POST",
+      body: JSON.stringify(values),
     });
 
     if (res) {
       goToAccount();
-      console.log('User registered');
+      console.log("User registered");
     } else {
-      console.error('Failed to register');
+      toast({
+        title: "Registrierung fehlgeschlagen",
+        description: "Bitte überprüfe deine Eingaben und versuche es erneut.",
+        variant: "destructive",
+      });
+      console.error("Failed to register");
     }
   } catch (error: any) {
+    toast({
+      title: "Registrierung fehlgeschlagen?",
+      description: "Bitte überprüfe deine Eingaben und versuche es erneut.",
+      variant: "destructive",
+    });
     fetchError.value = error.statusMessage;
   } finally {
     isLoading.value = false;
@@ -124,18 +171,28 @@ const registerSubmit = async (values: Record<string, any>) => {
 const loginSubmit = async (values: Record<string, any>) => {
   isLoading.value = true;
   try {
-    const res = await $fetch('/api/v1/auth/actions/login', {
-      method: 'POST',
-      body: JSON.stringify(values)
+    const res = await $fetch("/api/v1/auth/actions/login", {
+      method: "POST",
+      body: JSON.stringify(values),
     });
 
     if (res) {
       goToAccount();
-      console.log('User logged in');
+      console.log("User logged in");
     } else {
-      console.error('Failed to login');
+      toast({
+        title: "Login fehlgeschlagen",
+        description: "Bitte überprüfe deine Eingaben und versuche es erneut.",
+        variant: "destructive",
+      });
+      console.error("Failed to login");
     }
   } catch (error: any) {
+    toast({
+      title: "Login fehlgeschlagen?",
+      description: "Bitte überprüfe deine Eingaben und versuche es erneut.",
+      variant: "destructive",
+    });
     fetchError.value = error.statusMessage;
   } finally {
     isLoading.value = false;
@@ -143,6 +200,6 @@ const loginSubmit = async (values: Record<string, any>) => {
 };
 
 const goToAccount = () => {
-  useRouter().push('/myaccount');
+  useRouter().push("/myaccount");
 };
 </script>
